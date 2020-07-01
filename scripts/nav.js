1532ca8a06ce58a322139cdaae70e2ad50571d09
Jorengarenar <jorengarenar@protonmail.com>
2020-07-01 13:16:45 +0200

    Enriched experience

---
+++ scripts/nav.js
@@ -0,0 +1,31 @@
+let nav = document.createElement("nav");
+nav.innerHTML = `
+  <input id="hamburger" type="checkbox">
+  <label for="hamburger" class="hamburger"><i class="fas fa-bars"></i></label>
+  <div id="toggleTheme" class="switch">
+    <label>
+      <input type="checkbox" onclick="toggleTheme()">
+      <span class="slider"></span>
+    </label>
+    <i class="fas fa-sun"></i>
+  </div>
+  <a href="">HOME</a>
+  <a href="about">ABOUT</a>
+  <a href="projects">PROJECTS</a>
+  <a href="blog">BLOG</a>
+  <a href="mailto:jakublukasiewicz@o2.pl">CONTACT</a>
+`;
+document.body.appendChild(nav);
+
+if (document.querySelector("html").dataset.theme == "light") {
+  document.querySelector("nav input").checked = true;
+}
+
+let style = document.createElement("style");
+style.innerHTML = `
+  nav > a[href='` + document.currentScript.getAttribute("page") + `'] {
+    background-color: var(--bg);
+    color: var(--color);
+  }
+`
+document.body.appendChild(style);
