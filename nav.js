3a7d6c75f2a3ab9e436aefb3d5cc919005d44f14
Jorengarenar <dev@joren.ga>
2021-02-12 23:45:02 +0100

    Cleaning

---
+++ nav.js
@@ -0,0 +1,17 @@
+let nav = document.createElement("nav");
+nav.innerHTML = `
+  <a href="">HOME</a>
+  <a href="about">ABOUT</a>
+  <a href="projects">PROJECTS</a>
+  <a href="https://blog.joren.ga">BLOG</a>
+  <a href="mailto:mail@joren.ga">CONTACT</a>
+`;
+document.body.appendChild(nav);
+
+let style = document.createElement("style");
+style.innerHTML = `
+  nav > a[href='` + document.currentScript.getAttribute("page") + `'] {
+    color: var(--color);
+  }
+`
+document.body.appendChild(style);
