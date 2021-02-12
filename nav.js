5be4a302f2627ceb9a5a3af30c866fbad6aa89d9
Jorengarenar <dev@joren.ga>
2021-02-13 00:17:18 +0100

    Little tweak

--- nav.js
+++ nav.js
@@ -1,16 +1,20 @@
 let nav = document.createElement("nav");
 nav.innerHTML = `
+  <nobr>
   <a href="">HOME</a>
   <a href="about">ABOUT</a>
   <a href="projects">PROJECTS</a>
+  </nobr>
+  <nobr>
   <a href="https://blog.joren.ga">BLOG</a>
   <a href="mailto:mail@joren.ga">CONTACT</a>
+  </nobr>
 `;
 document.body.appendChild(nav);
 
 let style = document.createElement("style");
 style.innerHTML = `
-  nav > a[href='` + document.currentScript.getAttribute("page") + `'] {
+  nav a[href='` + document.currentScript.getAttribute("page") + `'] {
     color: var(--color);
   }
 `
